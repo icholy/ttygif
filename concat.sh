3eddd57050e0007b346e4d8c2afb0914279ecb25
Ilia Choly <icholy@accipiterradar.com>
2013-08-13 15:34:51 -0400

    fix concat script

--- concat.sh
+++ concat.sh
@@ -2,14 +2,18 @@
 
 output=${1-"output.gif"}
 
+if [ ! -e "$ttyfile" ]; then
+    echo "file $ttyfile does not exist"
+    exit
+fi
+
 gifs=$(find . -name '*.gif' | sort | xargs)
 
 _CONVERT="convert "
 
 for gif in $gifs; do
 
-    file=${gif##*/}
-    name=${file%.gif}
+    file=${gif##*/} name=${file%.gif}
     index=$(echo ${name%_*} | sed 's/0*//')
     delay=$((${name#*_}))
     ticks=$(echo "$delay * 0.1" | bc)
@@ -26,3 +30,4 @@ _CONVERT="${_CONVERT} -layers Optimize $output"
 echo "creating animated gif: $output"
 
 eval "$_CONVERT"
+
