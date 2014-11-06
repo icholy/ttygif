9541a1a942822dacf5f5227c001456b46f3d613d
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-11-06 14:00:35 +0100

    Add imagemagick and ttyrec as dependencies

--- ttygif.rb
+++ ttygif.rb
@@ -5,6 +5,9 @@ class Ttygif < Formula
     url 'https://github.com/icholy/ttygif/archive/1.0.8.zip'
     sha1 'f8d0a56af11d3ae8e2d5e64a4f6aceccb8338414'
 
+    depends_on 'imagemagick'
+    depends_on 'ttyrec'
+
     def install
         system "make"
         bin.install('ttygif')
